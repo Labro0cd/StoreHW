a483c21ef0ba4fdef5ccaa81572d208f0266f012
Drachlo <deuse.sec@gmail.com>
2025-01-12 15:21:16 +0300

    go

---
+++ src/main/java/com/example/storeHW/StoreHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storeHW/StoreHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storeHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
